151f370645813106356ab6932e4c050f8ba517fd
mpark <mpark4656@gmail.com>
2019-10-19 13:23:03 -0400

    Re-init

---
+++ chapter3/q3.cpp
@@ -0,0 +1,16 @@
+/*
+ * Created by mpark on 10/19/2019.
+ * Question: Write a program that doesn't do anything, but declares a number of variables with
+ * legal and illegal names (such as int double = 0;) so that you can see how the compiler reacts.
+ */
+#include <iostream>
+
+int main() {
+    int num = 7;    // No error
+    //int double = 0; // 'int' followed by 'double' is illegal, 'int': no variable declared before '='
+    //double 1t = 8;  // syntax error: 'user-defined literal'
+    //noType = 6;     // 'noType': undeclared identifier
+
+    return 0;
+}
+
