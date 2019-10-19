8b3f948098ba2d874d23a701ae57cfd67982c39a
mpark <mpark4656@gmail.com>
2019-10-19 13:29:29 -0400

    comments in q2.cpp

--- chapter3/q2.cpp
+++ chapter3/q2.cpp
@@ -1,6 +1,7 @@
 /*
  * Created by mpark on 10/19/2019.
- * Question: Write a program in C++ that converts from miles to kilometers. Your program should have a reasonable prompt for the user to enter a number of miles.
+ * Question: Write a program in C++ that converts from miles to kilometers.
+ * Your program should have a reasonable prompt for the user to enter a number of miles.
  * Hint: There are 1.609 kilometers to the mile
  */
 #include <iostream>
